9637f1749eab4d42b64a4401c4e2ee4f1d4251f6
qu4ll12 <lvs.hunghoa@gmail.com>
2025-10-01 23:25:14 +0700

    unit test for cupcake

--- unit4_pathway2_cupcake/build.gradle.kts
+++ unit4_pathway2_cupcake/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.testing)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
