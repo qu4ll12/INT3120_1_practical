1bc769fc11ac4371ce051ff181f0c6caaa2c0c19
qu4ll12 <lvs.hunghoa@gmail.com>
2025-10-15 18:39:04 +0700

    mars api

--- unit5_pathway1_marphotos/build.gradle.kts
+++ unit5_pathway1_marphotos/build.gradle.kts
@@ -41,6 +41,12 @@ android {
 
 dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
