a39d0348ccf510e4773e01f48326ccb2684083b9
qu4ll12 <lvs.hunghoa@gmail.com>
2025-10-15 17:38:27 +0700

    race tracker update

--- unit5_pathway1_racetracker/build.gradle.kts
+++ unit5_pathway1_racetracker/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
