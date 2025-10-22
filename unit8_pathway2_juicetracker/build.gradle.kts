0995d27cfbff2dd5229416ef196da4bd4e62002a
qu4ll12 <lvs.hunghoa@gmail.com>
2025-10-23 00:05:48 +0700

    pathway2 final

--- unit8_pathway2_juicetracker/build.gradle.kts
+++ unit8_pathway2_juicetracker/build.gradle.kts
@@ -43,10 +43,11 @@ android {
 dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.gms:play-services-base:18.5.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.android.gms:play-services-base:18.5.0")
+    implementation("com.google.android.gms:play-services-ads:23.3.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation(libs.androidx.core.ktx)
