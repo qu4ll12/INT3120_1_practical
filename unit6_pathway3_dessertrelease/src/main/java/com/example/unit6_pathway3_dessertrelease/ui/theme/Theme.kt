2a9413bfeec9af385e48f45a71954494c9fe0d55
qu4ll12 <lvs.hunghoa@gmail.com>
2025-10-22 16:59:33 +0700

    gradle change dessert release

--- unit6_pathway3_dessertrelease/src/main/java/com/example/unit6_pathway3_dessertrelease/ui/theme/Theme.kt
+++ unit6_pathway3_dessertrelease/src/main/java/com/example/unit6_pathway3_dessertrelease/ui/theme/Theme.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("DEPRECATION")
+
 package com.example.unit6_pathway3_dessertrelease.ui.theme
 
 import android.app.Activity
